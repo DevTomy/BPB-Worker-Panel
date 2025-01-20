832bb2579ec0bdad6d0a5e9d4b2ad05cd9b67c6d
Psrk Grmez <48916069+DevTomy@users.noreply.github.com>
2025-01-20 23:33:21 +0330

    test

--- src/pages/home.js
+++ src/pages/home.js
@@ -96,6 +96,7 @@ export async function renderHomePage (proxySettings, isPassSet) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <title>Collapsible Sections</title>
         <style>
+        
             :root {
                 --color: black;
                 --primary-color: #ff0000;
